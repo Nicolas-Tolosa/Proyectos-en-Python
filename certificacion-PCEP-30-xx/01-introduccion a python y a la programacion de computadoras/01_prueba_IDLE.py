15ba12829ace2a3e4b3ddbc742723eef42a6c79e
nicolasPC <nicolas.tolosar@gmail.com>
2024-10-29 20:00:58 -0300

    feat: agregar primer programa de pruebas para certificación en Python (Cisco) - incluye impresión en consola, pruebas de errores de sintaxis y análisis de traceback

---
+++ certificacion-PCEP-30-xx/01-introduccion a python y a la programacion de computadoras/01_prueba_IDLE.py
@@ -0,0 +1,19 @@
+# Primer programa antes de nuestro primer programa en Python
+print("Hisssssss...")
+
+# Esta es una prueba que generaría un error
+print"hissssssss..."
+
+# Esta es la última prueba para generar un mensaje de error
+prin("hisssssss...")
+
+# El mensaje de error muestra:
+
+# el traceback (que es la ruta que recorre el código a través de diferentes partes del programa; puedes ignorarlo por ahora, ya que está vacío en un código tan simple) ;
+
+# la ubicación del error (el nombre del archivo que contiene el error, el número de línea y el nombre del módulo); nota: el número puede ser engañoso, ya que Python suele mostrar el lugar donde notó por primera vez los efectos del error, no necesariamente el error en sí;
+
+# el contenido de la línea errónea; nota: la ventana del editor de IDLE no muestra números de línea, pero muestra la ubicación actual del cursor en la esquina inferior derecha; utilízalo para localizar la línea errónea en un código fuente largo;
+
+# el nombre del error y una breve explicación.
+
