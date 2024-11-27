d7e2ed5a08d57f616f738bceed46b1a819eb2d57
nicolasPC <nicolas.tolosar@gmail.com>
2024-11-26 22:30:16 -0300

    docs: update the documentation

---
+++ certificacion-PCEP-30-xx/02-tipos de datos, variables, operaciones basicas de entrada y salida, operadores basico/04_operadores_y_herramientas_de_manipulacion_de_datos.py
@@ -0,0 +1,14 @@
+# Python como una calculadora
+print(2+2) # resultado es 4
+
+# Operadores basicos
+# Operador +  : SUMA
+# Operador -  : RESTA
+# Operador *  : MULTIPLICADOR
+# Operador /  : DIVISOR
+# Operador // : DIVISION ENTERA
+# Operador %  : RESTO/MODULO
+# Operador ** : POTENCIA
+
+# Operadores y sus enlaces
+# El enlace de un operador determina el orden en que se computan las operaciones de los operadores con la misma prioridad, los cuales se encuentran dentro de una misma expresión.
---
+++ certificacion-PCEP-30-xx/02-tipos de datos, variables, operaciones basicas de entrada y salida, operadores basico/06_comentarios.py
@@ -0,0 +1,7 @@
+# Un texto insertado en el programa el cual es, omitido en la ejecución, es denominado un comentario.
+# En Python, un comentario es un texto que comienza con el símbolo # y se extiende hasta el final de la línea.
+# Se pueden comentar varias lineas a la vez, utilizando el atajo CTRL + /
+
+# Marcar fragmentos de codigo
+
+#Los comentarios pueden ser útiles en otro aspecto - se pueden utilizar para marcar un fragmento de código que actualmente no se necesita, cual sea la razón.
